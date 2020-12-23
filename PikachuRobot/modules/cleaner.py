741919afd32ad9c50bd6fba7b245b2b1deb6bb6c
Marshy <73180137+srinivasop@users.noreply.github.com>
2020-12-23 16:26:06 +0530

    Update cleaner.py

--- PikachuRobot/modules/cleaner.py
+++ PikachuRobot/modules/cleaner.py
@@ -1,11 +1,11 @@
 import html
 
-from SaitamaRobot import ALLOW_EXCL, CustomCommandHandler, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import (bot_can_delete,
+from PikachuRobot import ALLOW_EXCL, CustomCommandHandler, dispatcher
+from PikachuRobot.modules.disable import DisableAbleCommandHandler
+from PikachuRobot.modules.helper_funcs.chat_status import (bot_can_delete,
                                                            connection_status,
                                                            dev_plus, user_admin)
-from SaitamaRobot.modules.sql import cleaner_sql as sql
+from PikachuRobot.modules.sql import cleaner_sql as sql
 from telegram import ParseMode, Update
 from telegram.ext import (CallbackContext, CommandHandler, Filters,
                           MessageHandler, run_async)
