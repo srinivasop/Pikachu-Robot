eadd743761481df93c0bfadaaecad43d02f2d354
Marshy <73180137+srinivasop@users.noreply.github.com>
2020-12-23 16:45:52 +0530

    Update disasters.py

--- PikachuRobot/modules/disasters.py
+++ PikachuRobot/modules/disasters.py
@@ -3,12 +3,12 @@ import json
 import os
 from typing import Optional
 
-from SaitamaRobot import (DEV_USERS, OWNER_ID, DRAGONS, SUPPORT_CHAT, DEMONS,
+from PikachuRobot import (DEV_USERS, OWNER_ID, DRAGONS, SUPPORT_CHAT, DEMONS,
                           TIGERS, WOLVES, dispatcher)
-from SaitamaRobot.modules.helper_funcs.chat_status import (dev_plus, sudo_plus,
+from PikachuRobot.modules.helper_funcs.chat_status import (dev_plus, sudo_plus,
                                                            whitelist_plus)
-from SaitamaRobot.modules.helper_funcs.extraction import extract_user
-from SaitamaRobot.modules.log_channel import gloggable
+from PikachuRobot.modules.helper_funcs.extraction import extract_user
+from PikachuRobot.modules.log_channel import gloggable
 from telegram import ParseMode, TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 from telegram.utils.helpers import mention_html
