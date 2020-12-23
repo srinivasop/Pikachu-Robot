190443de3998a52c871b475c16755b7f0813eae2
Marshy <73180137+srinivasop@users.noreply.github.com>
2020-12-23 20:21:00 +0530

    Update gettime.py

--- PikachuRobot/modules/gettime.py
+++ PikachuRobot/modules/gettime.py
@@ -2,8 +2,8 @@ import datetime
 from typing import List
 
 import requests
-from SaitamaRobot import TIME_API_KEY, dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from PikachuRobot import TIME_API_KEY, dispatcher
+from PikachuRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
