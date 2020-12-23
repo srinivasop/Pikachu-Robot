48a8e151459a2aa4647751c981a161b241884b97
Marshy <73180137+srinivasop@users.noreply.github.com>
2020-12-23 16:00:50 +0530

    Update blacklist.py

--- PikachuRobot/modules/blacklist.py
+++ PikachuRobot/modules/blacklist.py
@@ -6,18 +6,18 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, MessageHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-import SaitamaRobot.modules.sql.blacklist_sql as sql
-from SaitamaRobot import dispatcher, LOGGER, REDIS
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from SaitamaRobot.modules.helper_funcs.extraction import extract_text
-from SaitamaRobot.modules.helper_funcs.misc import split_message
-from SaitamaRobot.modules.log_channel import loggable
-from SaitamaRobot.modules.warns import warn
-from SaitamaRobot.modules.helper_funcs.string_handling import extract_time
-from SaitamaRobot.modules.connection import connected
+import PikachuRobot.modules.sql.blacklist_sql as sql
+from PikachuRobot import dispatcher, LOGGER, REDIS
+from PikachuRobot.modules.disable import DisableAbleCommandHandler
+from PikachuRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from PikachuRobot.modules.helper_funcs.extraction import extract_text
+from PikachuRobot.modules.helper_funcs.misc import split_message
+from PikachuRobot.modules.log_channel import loggable
+from PikachuRobot.modules.warns import warn
+from PikachuRobot.modules.helper_funcs.string_handling import extract_time
+from PikachuRobot.modules.connection import connected
 
-from SaitamaRobot.modules.helper_funcs.alternate import send_message, typing_action
+from PikachuRobot.modules.helper_funcs.alternate import send_message, typing_action
 
 BLACKLIST_GROUP = 11
 
