dcc6dae55ace2e263044f17b521504f23fe91b66
Marshy <73180137+srinivasop@users.noreply.github.com>
2020-12-24 12:28:50 +0530

    Update paste.py

--- PikachuRobot/modules/paste.py
+++ PikachuRobot/modules/paste.py
@@ -1,6 +1,6 @@
 import requests
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from PikachuRobot import dispatcher
+from PikachuRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
