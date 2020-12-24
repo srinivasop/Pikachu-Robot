2a2bca6ae265f29a36d65e0bf06552c753e0e234
Dark Marshy <73180137+srinivasop@users.noreply.github.com>
2020-12-24 13:04:55 +0530

    Update users.py

--- PikachuRobot/modules/users.py
+++ PikachuRobot/modules/users.py
@@ -6,10 +6,10 @@ from telegram.error import BadRequest, Unauthorized
 from telegram.ext import (CallbackContext, CommandHandler, Filters,
                           MessageHandler, run_async)
 
-import SaitamaRobot.modules.sql.users_sql as sql
-from SaitamaRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
-from SaitamaRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
-from SaitamaRobot.modules.sql.users_sql import get_all_users
+import PikachuRobot.modules.sql.users_sql as sql
+from PikachuRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
+from PikachuRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from PikachuRobot.modules.sql.users_sql import get_all_users
 
 USERS_GROUP = 4
 CHAT_GROUP = 5
