b516bba28a136a83752f06b7716260e220c59c95
Marshy <73180137+srinivasop@users.noreply.github.com>
2020-12-24 12:36:22 +0530

    Update reactions.py

--- PikachuRobot/modules/reactions.py
+++ PikachuRobot/modules/reactions.py
@@ -1,7 +1,7 @@
 import random
 
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from PikachuRobot import dispatcher
+from PikachuRobot.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
