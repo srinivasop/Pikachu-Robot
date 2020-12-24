0379a09436e5ecccaa85b75ba3944acc3661f719
Marshy <73180137+srinivasop@users.noreply.github.com>
2020-12-24 12:37:52 +0530

    Update remote_cmds.py

--- PikachuRobot/modules/remote_cmds.py
+++ PikachuRobot/modules/remote_cmds.py
@@ -1,8 +1,8 @@
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.helper_funcs.chat_status import (
+from PikachuRobot import dispatcher
+from PikachuRobot.modules.helper_funcs.chat_status import (
     bot_admin, is_bot_admin, is_user_ban_protected, is_user_in_chat)
-from SaitamaRobot.modules.helper_funcs.extraction import extract_user_and_text
-from SaitamaRobot.modules.helper_funcs.filters import CustomFilters
+from PikachuRobot.modules.helper_funcs.extraction import extract_user_and_text
+from PikachuRobot.modules.helper_funcs.filters import CustomFilters
 from telegram import Update, ChatPermissions
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
