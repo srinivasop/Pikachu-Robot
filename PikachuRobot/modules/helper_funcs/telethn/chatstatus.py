7e3b0df17967226c0a33cae32430f6f34105b61d
Dark Marshy <73180137+srinivasop@users.noreply.github.com>
2020-12-25 10:41:05 +0530

    emm pros everywere yo

--- PikachuRobot/modules/helper_funcs/telethn/chatstatus.py
+++ PikachuRobot/modules/helper_funcs/telethn/chatstatus.py
@@ -1,5 +1,5 @@
-from SaitamaRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
-from SaitamaRobot import DRAGONS
+from PikachuRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
+from PikachuRobot import DRAGONS
 from telethon.tl.types import ChannelParticipantsAdmins
 
 
