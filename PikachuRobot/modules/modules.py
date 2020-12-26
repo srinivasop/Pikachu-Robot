3aaabf2d2ab74782b13c90258184f2f9a1c8eb5e
Kei <73867910+KeinShin@users.noreply.github.com>
2020-12-26 14:05:03 +0530

    Update modules.py

--- PikachuRobot/modules/modules.py
+++ PikachuRobot/modules/modules.py
@@ -19,7 +19,7 @@ def load(update: Update, context: CallbackContext):
         f"Attempting to load module : <b>{text}</b>", parse_mode=ParseMode.HTML)
 
     try:
-        imported_module = importlib.import_module("SaitamaRobot.modules." +
+        imported_module = importlib.import_module("PikachuRobot.modules." +
                                                   text)
     except:
         load_messasge.edit_text("Does that module even exist?")
